8b9532a4f9226106303e8c6148f7bf2022ebdb34
mactkg <mactkg@gmail.com>
2019-11-25 09:38:10 +0900

    Implemente simple commands

--- cmd/remo.go
+++ cmd/remo.go
@@ -1,12 +1,62 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 
 	"github.com/mactkg/remo"
 )
 
+var mainPostChannel string
+
+func myUsage() {
+	// TODO: refactor to struct `Action`
+	fmt.Printf("Usage: %s [OPTIONS] argument ...\n", os.Args[0])
+	fmt.Printf("Actions:\n")
+	fmt.Printf("  start,ohayo:\n\tStart remote working\n")
+	fmt.Printf("  pause:\n\tPause working\n")
+	fmt.Printf("  resume,unpause:\n\tResume working\n")
+	fmt.Printf("  move-office:\n\tStop remote work and move to office\n")
+	fmt.Printf("  finish,done,otsu:\n\tFinish working\n")
+
+	fmt.Printf("\nOptions:\n")
+	flag.PrintDefaults()
+}
+
 func main() {
-	cli := remo.New()
-	fmt.Printf("Hello, world! %v\n", cli)
+	flag.Usage = myUsage
+	flag.StringVar(&mainPostChannel, "mainPostCh", "", "where to post")
+	flag.Parse()
+
+	cmd := flag.Arg(0)
+	help := flag.Lookup("h")
+	if help != nil || cmd == "" {
+		flag.Usage()
+		return
+	}
+
+	token := os.Getenv("REMO_SLACK_TOKEN")
+	config := remo.Config{
+		SlackToken:      token,
+		MainPostChannel: mainPostChannel,
+	}
+
+	cli := remo.New(config)
+
+	switch cmd {
+	case "start":
+	case "ohayo":
+		cli.StartRemoteWork()
+	case "pause":
+		cli.PauseRemoteWork()
+	case "resume":
+	case "unpause":
+		cli.ResumeRemoteWork()
+	case "move-office":
+		cli.MoveToOffice()
+	case "done":
+	case "otsu":
+		cli.FinishRemoteWork()
+	}
 }
--- remo.go
+++ remo.go
@@ -1,7 +1,59 @@
 package remo
 
-type Remo struct{}
+type Remo struct {
+	slack  Slack
+	Config Config
+}
 
-func New() *Remo {
-	return &Remo{}
+type Config struct {
+	SlackToken      string
+	MainPostChannel string
+}
+
+func New(config Config) *Remo {
+	slack := NewSlack(config.SlackToken)
+	return &Remo{
+		slack:  slack,
+		Config: config,
+	}
+}
+
+func (r *Remo) StartRemoteWork() {
+	r.slack.SetStatus(Status{
+		Text:  "リモート中",
+		Emoji: ":male-technologist:",
+	})
+	r.slack.PostMessage(r.Config.MainPostChannel, "リモートワーク開始します")
+}
+
+func (r *Remo) PauseRemoteWork() {
+	r.slack.SetStatus(Status{
+		Text:  "離席中",
+		Emoji: ":tea:",
+	})
+	r.slack.PostMessage(r.Config.MainPostChannel, "休憩します")
+}
+
+func (r *Remo) ResumeRemoteWork() {
+	r.slack.SetStatus(Status{
+		Text:  "リモート中",
+		Emoji: ":male-technologist:",
+	})
+	r.slack.PostMessage(r.Config.MainPostChannel, "戻りました")
+}
+
+func (r *Remo) MoveToOffice() {
+	r.slack.SetStatus(Status{
+		Text:  "移動中",
+		Emoji: ":train:",
+	})
+	r.slack.PostMessage(r.Config.MainPostChannel, "リモートワーク終了して出社します")
+}
+
+func (r *Remo) FinishRemoteWork() {
+	r.slack.SetStatus(Status{
+		Text:  "閉店",
+		Emoji: ":crescent_moon:",
+	})
+	r.slack.PostMessage(r.Config.MainPostChannel, "リモートワーク終了します")
 }
