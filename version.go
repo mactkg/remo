64b1c9313adf8bbd8ef21f4404f19e42e2bbda96
mactkg <mactkg@gmail.com>
2021-03-10 10:29:43 +0900

    Bump up version to 0.3.0

--- version.go
+++ version.go
@@ -3,8 +3,8 @@ package remo
 import "fmt"
 
 var MAJOR=0
-var MINOR=2
-var PATCH=2
+var MINOR=3
+var PATCH=0
 var VERSION=fmt.Sprintf("%d.%d.%d", MAJOR, MINOR, PATCH)
 
 func GetCurrentVersion() string {
